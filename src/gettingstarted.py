9f26d91bd36113b762ae3339d6436b7d85232d93
someman Budhathoki <44607279+xettrisomeman@users.noreply.github.com>
2020-12-20 12:33:54 +0545

    part1 finished

--- src/gettingstarted.py
+++ src/gettingstarted.py
@@ -4,6 +4,16 @@ from fastapi import FastAPI
 app = FastAPI(debug=True)
 
 
+# root page
 @app.get("/")
 async def root():
     return {"message" : "Hello World"}
+
+
+# home page
+@app.get('/home')
+async def home():
+    return {"message" : "Home Page"}
+
+
+
